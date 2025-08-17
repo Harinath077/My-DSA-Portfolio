79336560476a75b1311c37bfd321fa25e60440f2
Harinath077 <harinathcreationz@gmail.com>
2025-08-18 00:05:02 +0530

    bubble sort

---
+++ My-DSA-Portfolio/Sorting Algos/Bubble Sort/bubbleor.java
@@ -0,0 +1,23 @@
+class Solution {
+    // Function to sort the array using bubble sort algorithm.
+    public void bubbleSort(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            boolean swapped = false;
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no elements were swapped, break early
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
---
+++ My-DSA-Portfolio/Sorting Algos/Selection Sort/selection_sort.py
@@ -0,0 +1,11 @@
+class Solution: 
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n-1):
+            min = i
+            for j in range(i+1,n):
+                if arr[min] > arr[j]:
+                    min = j
+            arr[i],arr[min] = arr[min],arr[i]
+                 
+        return arr
