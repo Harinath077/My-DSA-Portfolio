2404f3383db2306a882f000121ce38c4f4f38e68
Harinath077 <harinathcreationz@gmail.com>
2025-08-18 00:34:14 +0530

    chore: update documentation

---
+++ Sorting Algos/1. Selection Sort/SelectionSort.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Function to sort the array using selection sort algorithm
+    public void selectionSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; // assume the current index has the minimum
+            
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // update minIndex
+                }
+            }
+            
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
