e29ceb240ca82baf29fc78afc067de113e6d3a9d
Harinath077 <harinathcreationz@gmail.com>
2025-09-11 23:37:01 +0530

    update : optimalApproach

--- 2. Maths/3 .No of factors.py
+++ 2. Maths/3 .No of factors.py
@@ -5,3 +5,12 @@ class All_factors:
             if n % i == 0:
                 print(i,end=" ")
         print()
+    
+    def optimal(self,n):
+
+        for i in range(1, int(n**0.5) + 1):
+            if n % i == 0:
+                print(i, end=" ")
+                if i * i == n:
+                    continue
+                print(n // i, end=" ")
