1f18ed9dbbd3dd3c4707eaa18525e02db60be230
Harinath077 <harinathcreationz@gmail.com>
2025-09-12 22:55:24 +0530

    better appraoch

--- 2. Maths/4. Count Prime numbers.py
+++ 2. Maths/4. Count Prime numbers.py
@@ -18,4 +18,24 @@ class Bruteforce:
             results.append(count)
         return results
 
+class Better:
+    def sieve(self,N):
+        prime = [1] * (N + 1)
+        prime[0] = prime[1] = 0  # 0 and 1 are not prime
+        
+        for i in range(2, int(N**0.5) + 1):
+            if prime[i]:
+                for j in range(i * i, N + 1, i):
+                    prime[j] = 0
+        
+        return prime
+
+    def count_primes_better(self,queries, N=10**6):
+        prime = self.sieve(N)
+        results = []
+        for L, R in queries:
+            count = sum(prime[L:R+1])
+            results.append(count)
+        return results
+
 
