899ac48b207d49b1ae5777ea64f5a1ccb99c3175
Harinath077 <harinathcreationz@gmail.com>
2025-09-12 22:56:09 +0530

    optimal approach

--- 2. Maths/4. Count Prime numbers.py
+++ 2. Maths/4. Count Prime numbers.py
@@ -39,3 +39,37 @@ class Better:
         return results
 
 
+# Example Usage
+queries = [(3, 10), (2, 20), (1, 25)]
+obj = Better()
+print(obj.count_primes_better(queries))
+
+
+class Optimal:
+    def sieve_prefix(self,N):
+        prime = [1] * (N + 1)
+        prime[0] = prime[1] = 0  # 0 and 1 are not prime
+        
+        for i in range(2, int(N**0.5) + 1):
+            if prime[i]:
+                for j in range(i * i, N + 1, i):
+                    prime[j] = 0
+
+        # Compute prefix sum for fast range queries
+        prime_prefix = [0] * (N + 1)
+        for i in range(1, N + 1):
+            prime_prefix[i] = prime_prefix[i - 1] + prime[i]
+
+        return prime_prefix
+
+    def count_primes_optimal(self,queries, N=10**6):
+        prime_prefix = self.sieve_prefix(N)
+        results = []
+        for L, R in queries:
+            results.append(prime_prefix[R] - prime_prefix[L - 1])
+        return results
+
+# Example Usage
+queries = [(3, 10), (2, 20), (1, 25)]
+obj = Optimal()
+print(obj.count_primes_optimal(queries))
